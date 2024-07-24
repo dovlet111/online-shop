c19d64f79a8a4a5a267639c9ae1b4e701c92a82f
Dovlet <dovlrt2398@icloud.com>
2024-07-25 00:42:38 +0300

    fix

---
+++ app/Models/Product.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\Models\HasSlug;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Product extends Model
+{
+    use HasFactory;
+    use HasSlug;
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'brand_id',
+        'price',
+        'thumbnail'
+    ];
+
+    public function brand(): BelongsTo
+    {
+        return $this->belongsTo(Brand::class);
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
