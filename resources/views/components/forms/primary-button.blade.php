4ec6816b652f49f243f62085f4fcc9bd64537805
Dovlet <dovlrt2398@icloud.com>
2024-08-11 23:41:23 +0300

    fix

---
+++ resources/views/components/forms/primary-button.blade.php
@@ -0,0 +1,8 @@
+<button type="submit" class="w-full btn btn-pink">
+    {{ $slot }}
+</button>
+
+
+
+
+{{--<input type="password" class="w-full h-14 px-4 rounded-lg border border-[#A07BF0] bg-white/20 focus:border-pink focus:shadow-[0_0_0_2px_#EC4176] outline-none transition text-white placeholder:text-white text-xxs md:text-xs font-semibold" placeholder="Пароль" required>--}}
